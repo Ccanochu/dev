74ecccc5fe7780816a8e0a53fd921141fb0a0c68
Ccanochu <ccanochu@gmail.com>
2021-09-12 19:50:53 -0500

    callback and animation added

---
+++ js/06-callback.js
@@ -0,0 +1,30 @@
+jQuery(function(){
+    $('#animar').on('click',function(){    
+        
+        if (!$('#box').attr('style')) {
+            $(this).text("Desanimar")
+            animate()
+        }else{
+            $(this).text("Animar")          
+            noanimate()
+        }
+    })
+    
+    
+})
+function animate(){
+    $('#box').animate({marginLeft:'500px',
+                        fontSize:'30px',
+                        height:'60px',
+                        width:'350px',
+                        textAlign:'center',                        
+                        borderRadius:'900px'},'slow',function(){
+                            console.log("Caja animada")
+                        })
+}
+function noanimate(){
+    $('#box').removeAttr('style')
+}
+function slideToggle(){
+    $('#box').slideToggle('fast')
+}
