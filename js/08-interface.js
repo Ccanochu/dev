5ffc49d79d95bc59ae843739873e9870c89f3c3e
Ccanochu <ccanochu@gmail.com>
2021-09-13 13:16:04 -0500

    redizable draggable added

--- js/08-interface.js
+++ js/08-interface.js
@@ -1,5 +1,10 @@
 jQuery(function(){
     $('h1').animate({marginLeft:'500px'},'slow')
     $('.caja').animate({marginLeft:'100px'},'slow')
+
+    //mover los elementos
     $('.caja').draggable()
+    
+    //redimensionar
+    $('.caja').resizable()
 })
